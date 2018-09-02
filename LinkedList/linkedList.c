9f82a5df5fcc56e4945f9ca8ba60e8e675cfcc46
Jongwoo Kim <kjwoo810@gmail.com>
2018-09-02 22:48:03 +0900

    LinkedList: SLL(Single Linked List) realization complete

--- LinkedList/linkedList.c
+++ LinkedList/linkedList.c
@@ -48,6 +48,45 @@ void  SLL_AppendNode(Node** Head, Node* NewNode)
 	}
 }
 
+Node* SLL_GetNodeAt(Node* Head, int Location)
+{
+	Node* Current = Head;
+	while (Current != NULL && (--Location) >= 0)
+		Current = Current->NextNode;
+	return Current;
+}
+
+void SLL_RemoveNode(Node** Head, Node* Remove)
+{
+	if (*Head == Remove)
+		*Head = Remove->NextNode;
+	else
+	{
+		Node* Current = *Head;
+		while (Current != NULL && Current->NextNode != Remove)
+			Current = Current->NextNode;
+		if (Current != NULL)
+			Current->NextNode = Remove->NextNode;
+	}
+}
+
+void SLL_InsertAfter(Node* Current, Node* NewNode)
+{
+	NewNode->NextNode = Current->NextNode;
+	Current->NextNode = NewNode;
+}
+
+int SLL_GetNodeCount(Node* Head)
+{
+	int count = 0;
+	while (Head != NULL)
+	{
+		Head = Head->NextNode;
+		count++;
+	}
+	return count;
+}
+
 
 int main()
 {
